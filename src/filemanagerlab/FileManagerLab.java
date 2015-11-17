919a7df5ba4232c9432930186604db0655c73dac
dvandenberge <dvandenberge@L11703042766.wctc.msft>
2015-11-17 14:39:33 -0600

    fixed everything

--- src/filemanagerlab/FileManagerLab.java
+++ src/filemanagerlab/FileManagerLab.java
@@ -9,7 +9,7 @@ public class FileManagerLab {
     public static void main(String[] args) {
         FileManager m1=new FileManager();
 
-        m1.printEntry(1);
+        m1.printEntry(2);
 
     }
     
